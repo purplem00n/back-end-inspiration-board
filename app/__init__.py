89a91163c06575310b344da29e9b0dd84b14c26f
Elizabeth Valle <e7valle@gmail.com>
2023-06-26 19:07:11 -0700

    Register both boards and cards blueprint in dunder init file

--- app/__init__.py
+++ app/__init__.py
@@ -28,8 +28,11 @@ def create_app():
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    # from .routes import example_bp
-    # app.register_blueprint(example_bp)
+    from .routes import boards_bp
+    app.register_blueprint(boards_bp)
+
+    from .routes import cards_bp
+    app.register_blueprint(cards_bp)
 
     CORS(app)
     return app
