fdf0ebe451f6ef22b97d3033b8fc885ed6cfbd10
Ariel Higuera <purpl3m00ns@gmail.com>
2023-06-27 11:16:00 -0700

    Fix board relationship

--- app/models/board.py
+++ app/models/board.py
@@ -4,7 +4,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-    cards = db.Column(db.relationship("Card", back_populates="board", lazy=True))
+    cards = db.relationship("Card", back_populates="board", lazy=True)
 
     def to_dict(self):
         board_dict = {
